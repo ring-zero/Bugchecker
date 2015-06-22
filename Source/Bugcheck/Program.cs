b5ca8dc6b0426ad3ce9c2107a4e29fd3a14d530a
Patrick <patjbarker@gmail.com>
2015-06-22 13:42:59 -0400

    Fix
    Small misc changes to code, bug check updates.

--- Source/Bugcheck/Program.cs
+++ Source/Bugcheck/Program.cs
@@ -13,9 +13,10 @@ namespace Bugcheck
 
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             Console.Clear();
-            Dictionary<string, string> openWith = new Dictionary<string, string>
+            Dictionary<string, string> bugCheckDict = new Dictionary<string, string>
         {
-            { "0xa", "This indicates that Microsoft Windows or a kernel-mode driver accessed\n" + 
+            { "0xa", "IRQL_NOT_LESS_OR_EQUAL\n\n" + 
+                    "This indicates that Microsoft Windows or a kernel-mode driver accessed\n" + 
                     "paged memory at DISPATCH_LEVEL or above.\n\n" +
                     "What exactly does this mean?\n" +
                     "A Windows system driver (not 3rd party) or a 3rd party kernel-mode driver\n" + 
@@ -23,7 +24,8 @@ namespace Bugcheck
                     "and since MmAccessFault cannot handle pagefaults at IRQL 2 (on both x86/x64)\n" + 
                     "or higher, the 0xA bug check is called.\n" },
 
-            { "0xd1", "This indicates that a kernel-mode driver attempted to access pageable memory at\n" + 
+            { "0xd1", "DRIVER_IRQL_NOT_LESS_OR_EQUAL\n\n" + 
+                    "This indicates that a kernel-mode driver attempted to access pageable memory at\n" + 
                     "a process IRQL that was too high.\n\n" + 
                     "What exactly does this mean?\n" + 
                     "Similar to 0xA, 0xD1 is called when one of two things go wrong:\n\n" + 
@@ -39,21 +41,32 @@ namespace Bugcheck
                     "register and its contents are destroyed across a call by the caller.\n\n" + 
                     "Volatile registers: [rax], [rcx], [rdx], [r8], [r9], [r10:r11].\n"},
 
-            { "0x1", "This indicates that there has been a mismatch in the APC state index." },
+            { "0x1", "APC_INDEX_MISMATCH\n\n" + 
+                "This indicates that there has been a mismatch in the APC state index.\n\n" + 
+                "What exactly does this mean?\n" + 
+                "Well, it's unfortunately quite complicated. To understand this bug check, you\n" + 
+                "need to understand how APCs function. Once you've understood that, you need to\n" + 
+                "understand the types of APCs, as well as Critical and Guarded regions. To\n" + 
+                "explain that here would take the entire console window, so I'll just\n" + 
+                "recommend you check Windows Internals, or more specifically\n" + 
+                "Enrico Martignetti's paper on kernel APC internals.\n\n" + 
+                "In any case though, for a super simplified description for the sake of having\n" + 
+                "one, this bug check is mainly caused by 3rd party drivers causing mismatched\n" + 
+                "calls due to developmental oversight.\n"},
 
             { "0x2", "It indicates that a device queue was expected to be busy, but was not." },
 
             { "0x3", "It indicates a null of incorrect subset affinity." },
         };
 
-            Console.WriteLine("Type a bug check code (for example - 0xD1), or \"exit\" to quit:\n");
+            Console.WriteLine("Type a bug check code (for example - 0xD1), or \"exit\" to quit:");
 
             string userInput = "";
             while ((userInput = Console.ReadLine().ToLower()) != "exit")
             {
 
-                if (openWith.ContainsKey(userInput))
-                    Console.WriteLine(openWith[userInput]);
+                if (bugCheckDict.ContainsKey(userInput))
+                    Console.WriteLine(bugCheckDict[userInput]);
                 else
                     Console.WriteLine("Not a valid bug check!\n");
 
